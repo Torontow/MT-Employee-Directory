80cd03fecbc32445f140149a86f5a9a18d16b688
Torontow <mtorontow0@gmail.com>
2021-02-26 23:16:21 -0500

    Added Title component.

---
+++ src/components/Title/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Title() {
+  return (
+    <div
+      className="border-bottom border-warning border-5 py-4"
+      id="title-panel"
+    >
+      <h1>Employee Directory</h1>
+      <p>Search for a name or click the heading to sort by that heading.</p>
+    </div>
+  );
+}
+
+export default Title;
