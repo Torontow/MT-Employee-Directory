98caa2518e25188ed26b5aa7b2508a0eebb9bcb3
Torontow <mtorontow0@gmail.com>
2021-02-27 14:45:34 -0500

    Code refactor.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
