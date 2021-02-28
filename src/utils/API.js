7da2a40dada87b8adb7c9332a23e3d8288a2a28c
Torontow <mtorontow0@gmail.com>
2021-02-27 20:57:55 -0500

    Altered search to add more results.

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "https://randomuser.me/api/?results=100&nat=ca&inc=picture,name,phone,email,dob"
+const URL = "https://randomuser.me/api/?results=200&nat=ca&inc=picture,name,phone,email,dob"
 
 export default {
   getEmps: function() {
