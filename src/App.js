15fca2e24f72e72e323d71cde12bf351f80aefcd
Torontow <mtorontow0@gmail.com>
2021-02-27 20:56:24 -0500

    Added the search functionality to the application with handleinputchange.

--- src/App.js
+++ src/App.js
@@ -1,60 +1,67 @@
-import React, { Component } from "react"
-import Title from "./components/Title"
-import SearchPanel from "./components/SearchPanel"
-import List from "./components/List"
-import API from "./utils/API";
-
+import React, { Component } from 'react'
+import Title from './components/Title'
+import SearchPanel from './components/SearchPanel'
+import List from './components/List'
+import API from './utils/API'
 
 class App extends Component {
-  state = {
-    search: "",
-    results: [],
-    all: []
-  };
+  constructor (props) {
+    super(props)
 
-  // when this component mounts, display the random user list.
-
-  componentDidMount() {
+    this.state = {
+      search: '',
+      results: [],
+      all: []
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+  
+  componentDidMount () {
     this.getEmployeeList()
   }
 
-  getEmployeeList() {
+  getEmployeeList () {
     API.getEmps()
       .then(res => {
         let newRes = res.data.results.map((employee, index) => {
           return {
-            "picture": employee.picture.thumbnail,
-            "name": employee.name.first + " " + employee.name.last,
-            "phone": employee.phone,
-            "email": employee.email,
-            "dob": employee.dob.date.substring(0,10),
-            "id": index
+            picture: employee.picture.thumbnail,
+            name: employee.name.first + ' ' + employee.name.last,
+            phone: employee.phone,
+            email: employee.email,
+            dob: employee.dob.date.substring(0, 10),
+            id: index
           }
         })
         this.setState({ results: newRes, all: newRes })
       })
-      .catch(err => console.log(err));
-  };
+      .catch(err => console.log(err))
+  }
 
-  render() {
+  handleInputChange (event) {
+    const name = event.target.name
+    const value = event.target.value
+
+    this.setState({
+      results: this.state.all.filter(employee =>
+        employee.name.toLowerCase().includes(value.toLowerCase())
+      ),
+      [name]: value
+    })
+  }
+
+  render () {
     return (
       <div>
         <Title />
-        <SearchPanel 
+        <SearchPanel
           value={this.state.search}
           handleInputChange={this.handleInputChange}
         />
-        <List 
-        results={this.state.results}
-        />
-
+        <List results={this.state.results} />
       </div>
-
     )
   }
-
-
 }
 
-
-export default App;
+export default App
--- src/components/SearchPanel/index.js
+++ src/components/SearchPanel/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './style.css'
 
-function Search (props) {
+function SearchPanel (props) {
   return (
     <div className='row justify-content-center mb-4'id="search-panel-color">
       <div className='col-3 pt-3 pb-2'>
         <div className='input-group mb-3'>
           <input
             onChange={props.handleInputChange}
-            value={props.search}
+            value={props.value}
             name='search'
             type='text'
             className='form-control'
@@ -21,27 +21,6 @@ function Search (props) {
     </div>
   )
 
-  // return (
-  //   <form>
-  //     <div className="form-group">
-  //       <label htmlFor="search">Search:</label>
-  //       <input
-  //         onChange={props.handleInputChange}
-  //         value={props.search}
-  //         name="search"
-  //         type="text"
-  //         className="form-control"
-  //         placeholder="Search for a Gif"
-  //         id="search"
-  //       />
-  //       <button onClick={props.handleFormSubmit} className="btn btn-primary mt-3">
-  //         Search
-  //       </button>
-  //     </div>
-  //   </form>
-  // );
-
-
 }
 
-export default Search
+export default SearchPanel
